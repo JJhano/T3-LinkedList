2c96e7cefabc8b292d066a9f56daaf4da0a8bb0f
JJhano <jhanobk@gmail.com>
2024-04-25 15:27:03 -0400

    l

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
 CXX = g++
 CXXFLAGS = -std=c++11 -Wall
 
-SRCS =  main.cpp
-EXEC1 = main
+SRCS = permutacion.cpp main.cpp
+EXEC1 = permutacion
+EXEC2 = main
 
 .PHONY: all clean
 
-all: $(EXEC1)
+all: $(EXEC1) $(EXEC2)
+
+# Compilación de permutacion.cpp
+$(EXEC1): permutacion.cpp
+	$(CXX) $(CXXFLAGS) $^ -o $@
 
 # Compilación de main.cpp
-$(EXEC1): main.cpp
+$(EXEC2): main.cpp
 	$(CXX) $(CXXFLAGS) $^ -o $@
 
 clean:
-	rm -f $(EXEC1)
+	rm -f $(EXEC1) $(EXEC2)
--- permutacion.cpp
+++ permutacion.cpp
@@ -16,14 +16,13 @@ void insertInList(nodo **l, int num);
 bool removeFromList(nodo **l, int num);
 void printList(nodo *l);
 
-void appendList(nodo **l, nodo *q);
-void addPrevVal(nodo *l);
-void insertList(nodo **l, nodo *q);
-void insertListONE(nodo **l, nodo *q);
+bool existInList(nodo *l, int num);
 
+void printDecrecientes(nodo *l);
+void printCrecientes(nodo *l);
 int main(int argc, char **argv){
     if(argc != 3){
-        printf("Execute as ./main <n> <seed> \n");
+        printf("Execute as ./permutacion <n> <seed> \n");
         return EXIT_FAILURE;
     }
 	int n = atoi(argv[1]);
@@ -31,78 +30,90 @@ int main(int argc, char **argv){
 	srand(seed);
 
 	nodo *L = NULL;
-	nodo *Q = NULL;
-	// for(int i = 0; i < n; i++) appendToListR(&L, rand()%50);
-	for(int i = 0; i < n; i++) appendToListR(&Q, rand()%50);
-	printf("LISTA L:\n");
-	// printList(L);
-	// addPrevVal(L);
+	for(int i = 0; i < n; i++) {
+        int numero = 1 + (rand()%n);
+        while(existInList(L, numero)) numero = 1 + (rand()%n);
+        appendToListR(&L, numero);
+    }
+	printf("LISTA ORIGINAL L:\n");
+
 	printList(L);
 
-	printf("LISTA Q:\n");
+    printf("Sublistas Decrecientes\n");
+    printDecrecientes(L);
 
-	// printList(Q);
-	addPrevVal(Q);
-	printList(Q);
-
-	printf("LISTA L fusionada con Q:\n");
-	// insertListONE(&L, Q);
-	insertList(&L, Q);
-	printList(L);
-	// printf("LISTA Q sin modificaciones:\n");
-	// printList(Q);
+    printf("Sublistas Crecientes\n");
+    printCrecientes(L);
 	return EXIT_SUCCESS;
 }
 
-void addPrevVal(nodo *l){
+bool existInList(nodo *l, int num){
     nodo *p = l;
-    while(p->next != nullptr){
-        p->next->val += p->val;
+    while (p != nullptr){
+        if (p->val == num)
+            return true;
         p = p->next;
     }
+    return false;
 }
 
-void insertList(nodo **l, nodo *q){
-	nodo **p = l;
-	if(q == nullptr) return;
-	if(p == nullptr){
-		*p = q;
-		return;
-	}
-	nodo *r = q; //mantener apuntando a q
-	while(r  != nullptr){
-		nodo *nuevo = new nodo;
-		nuevo->val = r->val; //para no perder la lista q
-		nuevo->next = NULL;
-		while(*p != nullptr && (*p)->val < r->val){
-			p = &((*p)->next);
-		}
-		nuevo->next = *p;
-		*p = nuevo;
-
-		r = r->next;
-	}
-
-}
-
-void insertListONE(nodo **l, nodo *q) {
-    nodo **p = l;
-	if(q == nullptr) return;
-	if(p == nullptr){
-		*p = q;
-		return;
-	}
-    while (q != nullptr) {
-        nodo *nuevo = q;
-        q = q->next;
-        nuevo->next = NULL;
-        // Insertar el nuevo nodo en la lista l
-        while (*p != nullptr && (*p)->val < nuevo->val) {
-            p = &((*p)->next);
+void printDecrecientes(nodo *l){
+    nodo *p = l;
+    bool first = true;
+    while(p->next != nullptr){
+        if(p->val > p->next->val){
+            if(first){
+                printf("{%d,", p->val);
+                first = false;
+            }
+            else printf("%d", p->val);
         }
-        nuevo->next = *p;
-        *p = nuevo;
-    }
+        else if(first){
+            printf("{%d}", p->val);
+            first = true;
+        }
+        else{
+            printf(",%d}", p->val);
+            first = true;
+
+        }
+        p = p->next;
+    } 
+    if(p != nullptr && first) printf("{%d}", p->val);
+    
+    else if(p != nullptr) printf(",%d}", p->val);
+    printf("\n");
+    printf("\n");
+
+}
+
+void printCrecientes(nodo *l){
+    nodo *p = l;
+    bool first = true;
+    while(p->next != nullptr){
+        if(p->val < p->next->val){
+            if(first){
+                printf("{%d,", p->val);
+                first = false;
+            }
+            else printf("%d", p->val);
+        }
+        else if(first){
+            printf("{%d}", p->val);
+            first = true;
+        }
+        else{
+            printf(",%d}", p->val);
+            first = true;
+
+        }
+        p = p->next;
+    } 
+    if(p != nullptr && first) printf("{%d}", p->val);
+    
+    else if(p != nullptr) printf(",%d}", p->val);
+    printf("\n");
+    printf("\n");
 }
 
 // aniade un nodo al inicio de la lista --> Costo Constante = O(1)
@@ -162,6 +173,8 @@ void insertInList(nodo **l, int num){
 
 // elimina el primer nodo de la lista con valor num que encuentra y retorna true
 // (de izquierda a derecha). Si no lo encuentra retorna false
+
+
 bool removeFromList(nodo **l, int num){
 	if (*l == nullptr)
 		return false;
@@ -184,22 +197,6 @@ bool removeFromList(nodo **l, int num){
 	}	
 	
 	return false;
-}
-
-
-// Costo lineal : O(n)
-int minList(nodo *l){
-	if (l == nullptr){
-		printf("Lista Vacía !! ");
-		return -1;
-	}
-	int min = l->val;
-	while(l!=nullptr){
-		if (l->val < min)
-			min = l->val;
-		l = l->next;
-	}
-	return min;
 }
 
 
